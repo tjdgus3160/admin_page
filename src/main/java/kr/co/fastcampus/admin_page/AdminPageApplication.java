f59f88634da8c814841748101b360c2242f26285
tjdgus3160 <tjdgus3160@gmail.com>
2020-12-25 19:13:35 +0900

    first commit

---
+++ src/main/java/kr/co/fastcampus/admin_page/AdminPageApplication.java
@@ -0,0 +1,13 @@
+package kr.co.fastcampus.admin_page;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminPageApplication.class, args);
+	}
+
+}
